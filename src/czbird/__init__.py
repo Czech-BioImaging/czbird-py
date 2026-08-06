9575550837cf08fe2797a69451d5c6a94335ec8c
Vladimír Ulman <ulman@fi.muni.cz>
2026-08-07 01:09:22 +0200

    ADD: skeleton (moved from czbi_structural_model repo)

---
+++ src/czbird/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.0"
+version = __version__  # just to make the version info more visible/better accessible
+source_url = "https://github.com/Czech-BioImaging/czbird-py"
+docs_url = "https://github.com/Czech-BioImaging/czbird-py"
